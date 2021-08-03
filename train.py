10e47e8e8ef530f002984d792234ee7e76e2f49e
soohmatthew <matthewsoh18@gmail.com>
2021-08-03 14:32:20 +0800

    Updates

--- train.py
+++ train.py
@@ -1,3 +1,41 @@
+import time
+import logging
+import torch
+import random
+import numpy as np
+from tqdm import tqdm
+import datetime
+from sklearn.metrics import classification_report
+
+if torch.cuda.is_available():    
+
+        # Tell PyTorch to use the GPU.    
+        device = torch.device("cuda")
+
+        print('There are %d GPU(s) available.' % torch.cuda.device_count())
+
+        print('We will use the GPU:', torch.cuda.get_device_name(0))
+
+    # If not...
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+def format_time(elapsed):
+    '''
+    Takes a time in seconds and returns a string hh:mm:ss
+    '''
+    # Round to the nearest second.
+    elapsed_rounded = int(round((elapsed)))
+    
+    # Format as hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+
 def train_model(model,
                 optimizer,
                 scheduler,
