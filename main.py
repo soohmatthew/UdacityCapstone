607d27f71fcd13806732997c062b95624a9b01f7
Matthew Soh <matthewsoh18@gmail.com>
2021-08-04 14:46:07 +0800

    Updates

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import logging
 import os
 
 from utils import *
-from train import train_model
+from train import train_model_experiment, train_bert_model_experiment
 
 if __name__ == '__main__':
     # If there's a GPU available...
@@ -33,63 +33,9 @@ if __name__ == '__main__':
         print('No GPU available, using the CPU instead.')
         device = torch.device("cpu")
 
-    timing = str(strftime("%Y_%m_%d__%H_%M_%S", gmtime()))
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
+    max_length = 50
 
-    logging.basicConfig(filename = f"logs/base_test_{timing}.log",
-                        filemode='a',
-                                format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                                datefmt='%H:%M:%S',
-                                level=logging.INFO)
+    model = train_bert_model_experiment(max_length)
 
-    # Set variables
-    max_length = 30
-    learning_rate = 1e-5
-    epsilon = 1e-8 # args.adam_epsilon  - default is 1e-8.
-    epochs = 3
-
-    logging.info(f'Max length: {max_length}')
-    logging.info(f'Learning rate: {learning_rate}')
-    logging.info(f'Epsilon: {epsilon}')
-
-    # Baseline model
-
-    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
-
-    train_dataloader, validation_dataloader = create_data_loader(tokenizer, 
-                                                                max_length)
-
-    model = BertForSequenceClassification.from_pretrained("bert-base-multilingual-cased",
-                                                        num_labels = 3, # The number of output labels--2 for binary classification.
-                                                        output_attentions = False, # Whether the model returns attentions weights.
-                                                        output_hidden_states = False) # Whether the model returns all hidden-states.
-
-    logging.info(tokenizer)
-    logging.info(model)
-
-    model.cuda()
-    optimizer = AdamW(model.parameters(),
-                    lr = learning_rate, # args.learning_rate - default is 5e-5, our notebook had 2e-5
-                    eps = epsilon # args.adam_epsilon  - default is 1e-8.
-                    )
-
-    # Total number of training steps is [number of batches] x [number of epochs]. 
-    total_steps = len(train_dataloader) * epochs
-
-    # Create the learning rate scheduler.
-    scheduler = get_linear_schedule_with_warmup(optimizer, 
-                                                num_warmup_steps = 0, # Default value in run_glue.py
-                                                num_training_steps = total_steps)
-
-    model = train_model(model,
-                    optimizer,
-                    scheduler,
-                    train_dataloader,
-                    validation_dataloader,
-                    epochs
-                    )
-                    
-    model_path = f"{timing}_model.pt"
-    torch.save(model, model_path)
-    logging.shutdown()
+    for model_name in ["xlm-roberta-base", "distilbert-base-multilingual-cased", "xlm-roberta-large"]:
+        model = train_model_experiment(model_name, max_length)
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import time
+from time import strftime, gmtime
 import logging
 import torch
 import random
@@ -6,6 +6,9 @@ import numpy as np
 from tqdm import tqdm
 import datetime
 from sklearn.metrics import classification_report
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW, BertConfig, get_linear_schedule_with_warmup, AutoTokenizer, AutoModelForSequenceClassification
+from utils import create_data_loader
+import os
 
 if torch.cuda.is_available():    
 
@@ -89,249 +92,6 @@ def train_model(model,
       # For each batch of training data...
       for step, batch in enumerate(tqdm(train_dataloader)):
 
-          # # Progress update every 40 batches.
-          # if step % 40 == 0 and not step == 0:
-          #     # Calculate elapsed time in minutes.
-          #     elapsed = format_time(time.time() - t0)
-              
-          #     # Report progress.
-          #     print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
-
-          # Unpack this training batch from our dataloader. 
-          #
-          # As we unpack the batch, we'll also copy each tensor to the GPU using the 
-          # `to` method.
-          #
-          # `batch` contains three pytorch tensors:
-          #   [0]: input ids 
-          #   [1]: attention masks
-          #   [2]: labels 
-          b_input_ids = batch[0].to(device)
-          b_input_mask = batch[1].to(device)
-          b_labels = batch[2].to(device)
-
-          # Always clear any previously calculated gradients before performing a
-          # backward pass. PyTorch doesn't do this automatically because 
-          # accumulating the gradients is "convenient while training RNNs". 
-          # (source: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch)
-          model.zero_grad()        
-
-          # Perform a forward pass (evaluate the model on this training batch).
-          # In PyTorch, calling `model` will in turn call the model's `forward` 
-          # function and pass down the arguments. The `forward` function is 
-          # documented here: 
-          # https://huggingface.co/transformers/model_doc/bert.html#bertforsequenceclassification
-          # The results are returned in a results object, documented here:
-          # https://huggingface.co/transformers/main_classes/output.html#transformers.modeling_outputs.SequenceClassifierOutput
-          # Specifically, we'll get the loss (because we provided labels) and the
-          # "logits"--the model outputs prior to activation.
-          result = model(b_input_ids, 
-                        token_type_ids=None, 
-                        attention_mask=b_input_mask, 
-                        labels=b_labels,
-                        return_dict=True)
-
-          loss = result.loss
-          logits = result.logits
-
-          # Accumulate the training loss over all of the batches so that we can
-          # calculate the average loss at the end. `loss` is a Tensor containing a
-          # single value; the `.item()` function just returns the Python value 
-          # from the tensor.
-          total_train_loss += loss.item()
-
-          # Perform a backward pass to calculate the gradients.
-          loss.backward()
-
-          # Clip the norm of the gradients to 1.0.
-          # This is to help prevent the "exploding gradients" problem.
-          torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-
-          # Update parameters and take a step using the computed gradient.
-          # The optimizer dictates the "update rule"--how the parameters are
-          # modified based on their gradients, the learning rate, etc.
-          optimizer.step()
-
-          # Update the learning rate.
-          scheduler.step()
-
-      # Calculate the average loss over all of the batches.
-      avg_train_loss = total_train_loss / len(train_dataloader)            
-      
-      # Measure how long this epoch took.
-      training_time = format_time(time.time() - t0)
-
-      print("")
-      logging.info("  Average training loss: {0:.2f}".format(avg_train_loss))
-      logging.info("  Training epcoh took: {:}".format(training_time))
-          
-      # ========================================
-      #               Validation
-      # ========================================
-      # After the completion of each training epoch, measure our performance on
-      # our validation set.
-
-      print("")
-      logging.info("Running Validation...")
-
-      t0 = time.time()
-
-      # Put the model in evaluation mode--the dropout layers behave differently
-      # during evaluation.
-      model.eval()
-
-      # Tracking variables 
-      total_eval_accuracy = 0
-      total_eval_loss = 0
-      nb_eval_steps = 0
-
-      # Evaluate data for one epoch
-
-      epoch_pred = np.array([])
-      epoch_labels = np.array([])
-
-      for batch in validation_dataloader:
-          
-          # Unpack this training batch from our dataloader. 
-          #
-          # As we unpack the batch, we'll also copy each tensor to the GPU using 
-          # the `to` method.
-          #
-          # `batch` contains three pytorch tensors:
-          #   [0]: input ids 
-          #   [1]: attention masks
-          #   [2]: labels 
-          b_input_ids = batch[0].to(device)
-          b_input_mask = batch[1].to(device)
-          b_labels = batch[2].to(device)
-          
-          # Tell pytorch not to bother with constructing the compute graph during
-          # the forward pass, since this is only needed for backprop (training).
-          with torch.no_grad():        
-
-              # Forward pass, calculate logit predictions.
-              # token_type_ids is the same as the "segment ids", which 
-              # differentiates sentence 1 and 2 in 2-sentence tasks.
-              result = model(b_input_ids, 
-                            #token_type_ids=None, 
-                            attention_mask=b_input_mask,
-                            labels=b_labels,
-                            return_dict=True)
-
-          # Get the loss and "logits" output by the model. The "logits" are the 
-          # output values prior to applying an activation function like the 
-          # softmax.
-          loss = result.loss
-          logits = result.logits
-              
-          # Accumulate the validation loss.
-          total_eval_loss += loss.item()
-
-          # Move logits and labels to CPU
-          logits = logits.detach().cpu().numpy()
-          label_ids = b_labels.to('cpu').numpy()
-          pred_flat = np.argmax(logits, axis=1).flatten()
-
-          # Calculate the accuracy for this batch of test sentences, and
-          # accumulate it over all batches.
-          total_eval_accuracy += flat_accuracy(logits, label_ids)
-          
-          
-          epoch_pred = np.concatenate([epoch_pred, pred_flat])
-          epoch_labels = np.concatenate([epoch_labels, label_ids])
-
-      # Report the final accuracy for this validation run.
-      avg_val_accuracy = total_eval_accuracy / len(validation_dataloader)
-      logging.info("  Accuracy: {0:.2f}".format(avg_val_accuracy))
-
-      logging.info(f"Classification report: \n {classification_report(epoch_labels, epoch_pred)}")
-
-      # Calculate the average loss over all of the batches.
-      avg_val_loss = total_eval_loss / len(validation_dataloader)
-      
-      # Measure how long the validation run took.
-      validation_time = format_time(time.time() - t0)
-      
-      logging.info("  Validation Loss: {0:.2f}".format(avg_val_loss))
-      logging.info("  Validation took: {:}".format(validation_time))
-
-      # Record all statistics from this epoch.
-      training_stats.append(
-          {
-              'epoch': epoch_i + 1,
-              'Training Loss': avg_train_loss,
-              'Valid. Loss': avg_val_loss,
-              'Valid. Accur.': avg_val_accuracy,
-              'Training Time': training_time,
-              'Validation Time': validation_time
-          }
-      )
-
-  print("")
-  print("Training complete!")
-
-  logging.info("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
-
-  return(model)
-
-def train_distilbert_model(model,
-                optimizer,
-                scheduler,
-                train_dataloader,
-                validation_dataloader,
-                epochs
-                ):
-  # This training code is based on the `run_glue.py` script here:
-  # https://github.com/huggingface/transformers/blob/5bfcd0485ece086ebcbed2d008813037968a9e58/examples/run_glue.py#L128
-
-  # Set the seed value all over the place to make this reproducible.
-  seed_val = 42
-
-  random.seed(seed_val)
-  np.random.seed(seed_val)
-  torch.manual_seed(seed_val)
-  torch.cuda.manual_seed_all(seed_val)
-
-  # We'll store a number of quantities such as training and validation loss, 
-  # validation accuracy, and timings.
-  training_stats = []
-
-  # Measure the total training time for the whole run.
-  total_t0 = time.time()
-
-  # For each epoch...
-  for epoch_i in range(0, epochs):
-      
-      # ========================================
-      #               Training
-      # ========================================
-      
-      # Perform one full pass over the training set.
-
-      print("")
-      logging.info('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
-      logging.info('Training...')
-
-      # Measure how long the training epoch takes.
-      t0 = time.time()
-
-      # Reset the total loss for this epoch.
-      total_train_loss = 0
-
-      # Put the model into training mode. Don't be mislead--the call to 
-      # `train` just changes the *mode*, it doesn't *perform* the training.
-      # `dropout` and `batchnorm` layers behave differently during training
-      # vs. test (source: https://stackoverflow.com/questions/51433378/what-does-model-train-do-in-pytorch)
-      model.train()
-      
-      # For each batch of training data...
-      for step, batch in enumerate(tqdm(train_dataloader)):
-
-          # Unpack this training batch from our dataloader. 
-          #
-          # As we unpack the batch, we'll also copy each tensor to the GPU using the 
-          # `to` method.
-          #
           # `batch` contains three pytorch tensors:
           #   [0]: input ids 
           #   [1]: attention masks
@@ -503,3 +263,148 @@ def train_distilbert_model(model,
   logging.info("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
 
   return(model)
+
+def train_bert_model_experiment(max_length):
+    for handler in logging.root.handlers:
+        logging.root.removeHandler(handler)
+
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    timing = str(strftime("%Y_%m_%d__%H_%M_%S", gmtime()))
+
+    logger = logging.getLogger(f"logs/base_test_{timing}.log")
+
+    print( f"logs/base_test_{timing}.log")
+    logging.basicConfig(filename = f"logs/base_test_{timing}.log",
+                        filemode='a',
+                                format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                                datefmt='%H:%M:%S',
+                        level = logging.INFO)
+
+    # Set variables
+    learning_rate = 1e-5
+    epsilon = 1e-8 # args.adam_epsilon  - default is 1e-8.
+    epochs = 3
+
+    logging.info(f'Max length: {max_length}')
+    logging.info(f'Learning rate: {learning_rate}')
+    logging.info(f'Epsilon: {epsilon}')
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+
+    train_dataloader, validation_dataloader = create_data_loader(tokenizer, 
+                                                                max_length)
+
+
+    model = BertForSequenceClassification.from_pretrained("bert-base-multilingual-cased",
+                                                        num_labels = 3, # The number of output labels--2 for binary classification.
+                                                        output_attentions = False, # Whether the model returns attentions weights.
+                                                        output_hidden_states = False) # Whether the model returns all hidden-states.
+
+    logging.info(tokenizer)
+    logging.info(model)
+
+    model.cuda()
+    optimizer = AdamW(model.parameters(),
+                        lr = learning_rate, # args.learning_rate - default is 5e-5, our notebook had 2e-5
+                        eps = epsilon # args.adam_epsilon  - default is 1e-8.
+                    )
+
+
+    # Total number of training steps is [number of batches] x [number of epochs]. 
+    total_steps = len(train_dataloader) * epochs
+
+    # Create the learning rate scheduler.
+    scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                                num_warmup_steps = 0, # Default value in run_glue.py
+                                                num_training_steps = total_steps)
+
+    model = train_model(model,
+                    optimizer,
+                    scheduler,
+                    train_dataloader,
+                    validation_dataloader
+                    )
+
+    if not os.path.exists("model"):
+        os.makedirs("model")
+
+    torch.save(model, f"model/bert-base-multilingual-cased-{timing}.pt")
+
+    logging.shutdown()
+
+    return model
+
+def train_model_experiment(model_name, max_length):
+
+    for handler in logging.root.handlers:
+        logging.root.removeHandler(handler)
+
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    timing = str(strftime("%Y_%m_%d__%H_%M_%S", gmtime()))
+
+    logger = logging.getLogger(f"logs/base_test_{timing}.log")
+
+    print( f"logs/base_test_{timing}.log")
+    logging.basicConfig(filename = f"logs/base_test_{timing}.log",
+                        filemode='a',
+                                format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                                datefmt='%H:%M:%S',
+                        level = logging.INFO)
+
+    # Set variables
+    learning_rate = 1e-5
+    epsilon = 1e-8 # args.adam_epsilon  - default is 1e-8.
+    epochs = 3
+
+    logging.info(f'Max length: {max_length}')
+    logging.info(f'Learning rate: {learning_rate}')
+    logging.info(f'Epsilon: {epsilon}')
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    train_dataloader, validation_dataloader = create_data_loader(tokenizer, 
+                                                                max_length)
+
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name,
+                                                            num_labels = 3, # The number of output labels--2 for binary classification.
+                                                            output_attentions = False, # Whether the model returns attentions weights.
+                                                            output_hidden_states = False)
+
+    logging.info(tokenizer)
+    logging.info(model)
+
+    model.cuda()
+    optimizer = AdamW(model.parameters(),
+                        lr = learning_rate, # args.learning_rate - default is 5e-5, our notebook had 2e-5
+                        eps = epsilon # args.adam_epsilon  - default is 1e-8.
+                    )
+
+
+    # Total number of training steps is [number of batches] x [number of epochs]. 
+    total_steps = len(train_dataloader) * epochs
+
+    # Create the learning rate scheduler.
+    scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                                num_warmup_steps = 0, # Default value in run_glue.py
+                                                num_training_steps = total_steps)
+
+    model = train_model(model,
+                    optimizer,
+                    scheduler,
+                    train_dataloader,
+                    validation_dataloader
+                    )
+
+    if not os.path.exists("model"):
+        os.makedirs("model")
+
+    torch.save(model, f"model/{model_name}-{timing}.pt")
+
+    logging.shutdown()
+
+    return model
