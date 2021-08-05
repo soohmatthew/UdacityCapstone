a4477c8f3af061bbb98c719eb2434c20ea7c4c9b
Matthew Soh <matthewsoh18@gmail.com>
2021-08-05 23:15:23 +0800

    Updates

--- generate_test.py
+++ generate_test.py
@@ -2,7 +2,7 @@ from utils import *
 from transformers import AutoTokenizer
 
 if __name__ == "__main__":
-    model_path = "<<PATH TO TRAINED MODEL>>"
+    model_path = "model/xlm-roberta-large-2021_08_04__14_34_28.pt"
     tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large")
     max_length = 30
     df = get_test_results(model_path, tokenizer, max_length)
